ab1b8040220785d92136b640a9fea8474fb75f0d
Chris Zhang <clz16@case.edu>
2018-04-21 17:16:30 -0400

    skeleton, autogenerated files

---
+++ EventApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventApp
+//
+//  Created by Christopher Zhang on 4/21/18.
+//  Copyright © 2018 Christopher Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
